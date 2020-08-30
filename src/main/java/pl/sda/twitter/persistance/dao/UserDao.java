e6f81b7cbaa02f1d13fc739f5356c70ea42b9f5b
piotr <piomal3@gmail.com>
2020-08-30 10:11:27 +0200

    integracja z bazą danych

--- src/main/java/pl/sda/twitter/persistance/dao/UserDao.java
+++ src/main/java/pl/sda/twitter/persistance/dao/UserDao.java
@@ -5,17 +5,25 @@ import org.hibernate.query.Query;
 import pl.sda.twitter.persistance.HibernateUtil;
 import pl.sda.twitter.persistance.entities.TbUser;
 
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+
 //DAO data access object
 public class UserDao {
 
-    public TbUser getUserByLogin(String login){
-        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+    public TbUser getUserByLogin(String login) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query q = session.createQuery(
-                    "SELECT e FROM TbUser e WHERE e.login=:username");
+                    "SELECT e FROM " + TbUser.class.getName() +" e WHERE e.login=:username");
             q.setParameter("username", login);
             session.beginTransaction();
-            q.getSingleResult();
-            session.close();
+            TbUser singleResult = (TbUser) q.getSingleResult();
+            session.getTransaction().commit();
+            return singleResult;
+        } catch (NoResultException e) {
+            return null;
+        } catch (NonUniqueResultException e) {
+            return null;
         }
     }
 }
--- src/main/java/pl/sda/twitter/services/UserService.java
+++ src/main/java/pl/sda/twitter/services/UserService.java
@@ -1,17 +1,17 @@
 package pl.sda.twitter.services;
 
+import pl.sda.twitter.persistance.dao.UserDao;
 import pl.sda.twitter.persistance.entities.TbUser;
 
 public class UserService {
 
+    private UserDao userDao = new UserDao();
+
     public TbUser getUser(String userName, String userPassword) {
-        if (userName.equals("admin") && userPassword.equals("password")) {
-            return TbUser.builder()
-                    .login(userName)
-                    .password(userPassword)
-                    .build();
-        } else {
+        TbUser user = userDao.getUserByLogin(userName);
+        if (user == null || !user.getPassword().equals(userPassword)){
             return null;
         }
+        return user;
     }
 }
